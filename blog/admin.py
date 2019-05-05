0aba9f048e8c0d806d4f58613d4f7948bd3edef4
lidawei <lidawei918@163.com>
2019-05-05 18:08:04 +0800

    admin

--- blog/admin.py
+++ blog/admin.py
@@ -10,51 +10,51 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ("name", "status", "is_nav", "created_time")
     fields = ("name", "status", "is_nav", "owner")
 
-    def save_model(self, request, obj, form, change):
-        obj.owner = request.user
-        return super(CategoryAdmin, self).save_model(request, obj, form, change)
+    # def save_model(self, request, obj, form, change):
+    #     obj.owner = request.user
+    #     return super(CategoryAdmin, self).save_model(request, obj, form, change)
 
 
-@admin.register(Tag)
-class TagAdmin(admin.ModelAdmin):
-    list_display = ("name", "status", "created_time")
-    fields = ("name", "status")
+# @admin.register(Tag)
+# class TagAdmin(admin.ModelAdmin):
+#     list_display = ("name", "status", "created_time")
+#     fields = ("name", "status")
+#
+#     def save_model(self, request, obj, form, change):
+#         obj.owner = request.owner
+#         return super(TagAdmin, self).save_model(request, obj, form, change)
 
-    def save_model(self, request, obj, form, change):
-        obj.owner = request.owner
-        return super(TagAdmin, self).save_model(request, obj, form, change)
 
-
-@admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
-    list_display = [
-        "title", "category", "status", "created_time",
-    ]
-    list_display_links = []
-    list_filter = ["category"]
-    search_fields = ["title", "category_name"]
-    actions_on_top = True
-    actions_on_bottom = True
-
-    # 编辑页面
-    save_on_top = True
-    fields = (
-        ("category", "title"),
-        "desc",
-        "status",
-        "content",
-        "tag",
-        "owner"
-    )
-
-    def operator(self, obj):
-        return format_html(
-            "<a href = '{}'>编辑</a>",
-            reverse("admin:blog_post_change", args=(obj.id))
-        )
-
-    operator.short_description = "操作"
-
-    def save_model(self, request, obj, form, change):
-        obj.owner = request.user
-        return super(PostAdmin, self).save_model(request, obj, form, change)
+# @admin.register(Post)
+# class PostAdmin(admin.ModelAdmin):
+#     list_display = [
+#         "title", "category", "status", "created_time",
+#     ]
+#     list_display_links = []
+#     list_filter = ["category"]
+#     search_fields = ["title", "category_name"]
+#     actions_on_top = True
+#     actions_on_bottom = True
+#
+#     # 编辑页面
+#     save_on_top = True
+#     fields = (
+#         ("category", "title"),
+#         "desc",
+#         "status",
+#         "content",
+#         "tag",
+#         "owner"
+#     )
+#
+#     def operator(self, obj):
+#         return format_html(
+#             "<a href = '{}'>编辑</a>",
+#             reverse("admin:blog_post_change", args=(obj.id))
+#         )
+#
+#     operator.short_description = "操作"
+#
+#     def save_model(self, request, obj, form, change):
+#         obj.owner = request.user
+#         return super(PostAdmin, self).save_model(request, obj, form, change)
